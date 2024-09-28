cfa5eb9d8b7cf64f294a98909fc3c2d9af95f30d
Rakesh-608 <rakeshchenna2002@gmail.com>
2024-09-28 06:31:37 +0530

    incident service modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { Router, NavigationEnd } from '@angular/router'; // Import Router and NavigationEnd
 import { UserNavbarComponent } from './components/user-components/user-navbar/user-navbar.component';
+import { UserService } from './services/user.service';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -22,4 +23,64 @@ export class AppComponent {
       }
     });
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  // isLoggedIn: boolean = false;
+
+  // constructor(private userService: UserService, private renderer: Renderer2) {}
+
+  // // ngOnInit(): void {
+  // //   // Subscribe to the observable and update isLoggedIn accordingly
+  // //   this.userService.isLoggedIn().subscribe(
+  // //     (loggedIn: boolean) => {
+  // //       this.isLoggedIn = loggedIn;
+  // //     },
+  // //     (error) => {
+  // //       console.error('Error checking login status', error);
+  // //     }
+  // //   );
+  // // }
+
+  // ngOnInit(): void {
+  //   // Subscribe to the observable and update isLoggedIn accordingly
+  //   this.userService.isLoggedIn().subscribe(
+  //     (loggedIn: boolean) => {
+  //       this.isLoggedIn = loggedIn;
+  //       // Add or remove CSS class based on the login status
+  //       if (loggedIn) {
+  //         this.renderer.addClass(document.body, 'with-navbar');
+  //         this.renderer.removeClass(document.body, 'no-navbar');
+  //       } else {
+  //         this.renderer.addClass(document.body, 'no-navbar');
+  //         this.renderer.removeClass(document.body, 'with-navbar');
+  //       }
+  //     },
+  //     (error) => {
+  //       console.error('Error checking login status', error);
+  //     }
+  //   );
+  // }
 }
